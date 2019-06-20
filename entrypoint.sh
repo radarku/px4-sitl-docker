892bcbed48cbae47bfb91d05e4b9eb371df71547
Kyle Usbeck <radarku@gmail.com>
2019-06-20 09:34:57 -0400

    video out not working yet don't bother running socat yet

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 HOST_IP=$1
-socat UDP4-RECVFROM:5600,fork UDP4-SENDTO:${HOST_IP}:5600 &
+#socat UDP4-RECVFROM:5600,fork UDP4-SENDTO:${HOST_IP}:5600 &
 python3 /usr/local/bin/mavproxy.py --daemon --master=udpin:127.0.0.1:14550 --out=tcpin:0.0.0.0:5760 2>&1 > /dev/null &
 HEADLESS=1 make px4_sitl_default gazebo
